992bf068414f65dc3a23b2aaabc266982e42f476
Mattéo <43877671+MatteoLB@users.noreply.github.com>
2019-04-26 14:27:27 +0200

    ajout suivi de la main pour le joueur

--- game.js
+++ game.js
@@ -12,8 +12,10 @@ const empty = 0;	// valeurs contenues dans le tableau représentant la map du je
 const obstacle = 1;
 const player = 2;
 
-let playerX = laneWidth;	// positions horizontales et verticales du joueur au début de la partie (couloir du milieu et un peu au dessus du bas)
+let playerX;	// positions horizontales et verticales du joueur au début de la partie (couloir du milieu et un peu au dessus du bas)
 let playerY = canvas.height - 90;
+
+let playerWidth = laneWidth/3;
 
 let currentObstacleCount = 0;	// nombre actuel d'obstacles générés
 let maxObstacleCount = 8;		// nombre maximum d'obstacles présentes en même temps
@@ -35,7 +37,7 @@ for (var i = 0; i < laneNumber; i++) // initialise le tableau de la map en metta
 	}
 }
 
-map[1][playerY/caseSize] = player; // place le joueur dans l'array de la map
+// map[1][playerY/caseSize] = player; // place le joueur dans l'array de la map ***************************************************
 
 
 
@@ -71,6 +73,16 @@ function getPlayerLocation(handData) // récupère le couloir actuel du joueur s
 }
 
 
+function getPlayerLocationV2(handData)
+{
+	// les coordonnées récupérées correspondent au haut/gauche du carré bleu affiché autour de la main
+	// récupère la position au centre de ce carré en ajoutant la moitié de la hauteur ou largeur du carré
+	playerX = handData[0] + handData[2]/2 - playerWidth/2; 
+
+	// let pointerY = handData[1] + handData[3]/2; // position y n'est utilisé que pour le pointeur, à supprimer si on ne dessine plus le pointeur
+	// drawPointer(playerX, pointerY+100);
+}
+
 
 function drawObstacle(x, y)						// dessine chaque obstacle selon ses coordonnées x et y
 {
@@ -81,7 +93,8 @@ function drawObstacle(x, y)						// dessine chaque obstacle selon ses coordonné
 	ctx.closePath();
 }
 
-function drawPointer(x, y)	// dessine un pointeur indiquant la position de la main
+
+function drawPointer(x, y) // dessine un pointeur indiquant la position de la main
 {
 	ctx.beginPath();
 	ctx.rect(x, y, caseSize, caseSize);
@@ -90,11 +103,12 @@ function drawPointer(x, y)	// dessine un pointeur indiquant la position de la ma
 	ctx.closePath();
 }
 
+
 /* A 1 chance sur N de générer un nouvel obstacle (sauf si il n'y a pas encore d'obstacle sur la map), à condition qu'un obstacle n'ait pas déjà
    été ajouté lors de la frame précédente (pour éviter d'avoir des obstacles successifs impossibles à passer */
 function newObstacle(obstacleProba)
 {									
-	if ((getRandomInt(obstacleProba) == 1 || currentObstacleCount <= 1) && framesSinceLastObstacle > 1) 
+	if ((getRandomInt(obstacleProba) == 1 || currentObstacleCount <= 1) && framesSinceLastObstacle > 3) 
 	{
 		let random = getRandomInt(laneNumber); // tire le couloir au sort, ajoute l'obstacle à l'array map et le dessine sur le canvas
 		map[random][0] = obstacle;
@@ -103,6 +117,7 @@ function newObstacle(obstacleProba)
 		framesSinceLastObstacle = 0;
 	}
 }
+
 
 function moveObstacles() // descend tous les obstacles d'un cran et les redessine à leur nouvelle position
 {
@@ -130,14 +145,57 @@ function moveObstacles() // descend tous les obstacles d'un cran et les redessin
 	} // fin for (lanes)
 } // fin fonction
 
+
+function moveObstaclesV2() // descend tous les obstacles d'un cran et les redessine à leur nouvelle position
+{
+	let collision = false;
+
+	for (var i = 0; i < map.length; i++) // parcourt chaque couloir
+	{
+		for (var j = map[i].length - 1; j > 0; j--) // puis chaque case de chaque couloir, en partant de la fin
+		{
+			if (map[i][j-1] == obstacle) // si le prochain élément est un obstacle, on le déplace dans l'élément courant
+			{
+				collision = detectCollision(i*laneWidth, j*caseSize); // détecte si il y a collision entre l'élément courant et le joueur
+
+				if (!collision) // si il n'y a pas de collision
+				{
+					map[i][j] = obstacle;
+					map[i][j-1] = empty;
+
+					drawObstacle(i*laneWidth, j*caseSize);
+					currentObstacleCount++; 	// compte le nombre d'obstacles actuellement présents sur la map
+				}
+
+			} // fin if obstacle
+		} // fin for (rows)
+	} // fin for (lanes)
+} // fin fonction
+
+
+function detectCollision(x, y) // détecte une collision entre le joueur et un obstacle, prend x et y de l'obstacle vérifié en paramètres
+{
+	// si la position verticale est la même, et que la position x du côté gauche ou droite du joueur se trouve entre la position x gauche
+	// ou droite de l'obstacle
+	if (y == playerY && ((playerX >= x && playerX <= x + laneWidth) || (playerX + playerWidth >= x && playerX + playerWidth <= x + laneWidth)) ) 
+	{
+		gameStatus = 0; // game over
+		return true; // collision vaudra vrai
+	}
+	return false; // sinon collision vaudra faux
+}
+
+
 function drawPlayer() // dessine un rectangle en guise de joueur
 {
 	ctx.beginPath();
-	ctx.rect(playerX+laneWidth/4, playerY, laneWidth/2, caseSize);
+	//ctx.rect(playerX+laneWidth/4, playerY, playerWidth, caseSize);
+	ctx.rect(playerX, playerY, playerWidth, caseSize);
 	ctx.fillStyle = "red";
 	ctx.fill();
 	ctx.closePath();
 }
+
 
 function gameOver()	// affiche game over
 {
@@ -148,6 +206,7 @@ function gameOver()	// affiche game over
 	ctx.closePath();
 }
 
+
 function mainGame(handData) // fonction principale, reçoit les coordonnées de la main en paramètre
 {
 	ctx.clearRect(0, 0, canvas.width, canvas.height); // efface tout le canvas
@@ -155,8 +214,8 @@ function mainGame(handData) // fonction principale, reçoit les coordonnées de 
 	currentObstacleCount = 0; // remet le compteur d'obstacles à 0 (ceux-ci sont comptés lorsqu'ils sont déplacés et dessinés)
 	framesSinceLastObstacle++;	// incrémente le compteur de frames depuis le dernier obstacle
 
-	getPlayerLocation(handData); // récupère la position du joueur
-	moveObstacles(); 			 // déplace les obstacles et les redessine
+	getPlayerLocationV2(handData); // récupère la position du joueur
+	moveObstaclesV2(); 			 // déplace les obstacles et les redessine
 
 	if (currentObstacleCount < maxObstacleCount) // si le nombre actuel d'obstacles est inférieur au max
 	{
